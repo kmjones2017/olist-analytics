d5dcaf8d3c9e5cd308d636a34f7f31ffb3090f65
Kayla Jones <kmjoness2017@gmail.com>
2026-04-07 01:48:21 +0000

    created geolocation staging model and added documentation for it to _stg_olist.yml

---
+++ models/staging/stg_olist__geolocation.sql
@@ -0,0 +1,17 @@
+with source as (
+    select * from {{ source('olist', 'geolocation') }}
+),
+
+transformed as (
+    select
+
+        cast(geolocation_zip_code_prefix as varchar) as zip_code_prefix,
+        cast(geolocation_lat as float) as latitude,
+        cast(geolocation_lng as float) as longitude,
+        trim(geolocation_city) as city,
+        trim(geolocation_state) as state 
+        
+    from source 
+)
+
+select * from transformed
