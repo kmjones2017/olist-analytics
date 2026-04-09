d840155e831418d7406890882adefca8d0af81c3
Kayla Jones <kmjoness2017@gmail.com>
2026-04-09 17:54:03 +0000

    created and documented intermediate model, and edited upstream models for consistency

---
+++ models/intermediate/int_order_items_enriched.sql
@@ -0,0 +1,37 @@
+with orders as (
+
+    select * from {{ ref('stg_olist__orders') }}
+
+),
+
+order_items as (
+
+    select * from {{ ref('stg_olist__order_items') }}
+
+),
+
+int_order_items_enriched as (
+
+    select 
+
+        orders.order_id,
+        order_items.order_item_id,
+        order_items.product_id,
+        orders.customer_id,
+        order_items.seller_id,
+        orders.order_status,
+        order_items.item_price,
+        order_items.freight_value,
+        orders.order_purchase_timestamp,
+        orders.order_estimated_delivery_date,
+        orders.order_approved_at,
+        order_items.seller_shipping_deadline,
+        orders.order_delivered_carrier_date,
+        orders.order_delivered_customer_date
+
+    from order_items
+    where orders.order_status not in ('canceled', 'unavailable')
+
+)
+
+select * from int_order_items_enriched
