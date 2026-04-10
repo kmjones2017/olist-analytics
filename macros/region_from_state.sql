b623e1422ef8d2f2fec3bfbeb6f6023019ec58bb
Kayla Jones <kmjoness2017@gmail.com>
2026-04-10 01:43:16 +0000

    fixed select statement in dim_customers and added a comment to region_from_state macro file

--- macros/region_from_state.sql
+++ macros/region_from_state.sql
@@ -1,12 +1,16 @@
 {% macro region_from_state(state_column) %}
+    /*
+    Maps Brazilian state abbreviations to geographic regions.
+    Used in customer and seller dimension models for regional aggregation.
+    */
 
-case 
-    when {{ state_column }} in ('SP','RJ','MG','ES') then 'Southeast'
-    when {{ state_column }} in ('PR','SC','RS') then 'South'
-    when {{ state_column }} in ('BA','PE','CE','RN','PB','AL','SE','MA','PI') then 'Northeast'
-    when {{ state_column }} in ('AM','PA','RO','RR','AP','TO','AC') then 'North'
-    when {{ state_column }} in ('GO','MT','MS','DF') then 'Central-West'
-    else 'Unknown'
-end
+    case 
+        when {{ state_column }} in ('SP','RJ','MG','ES') then 'Southeast'
+        when {{ state_column }} in ('PR','SC','RS') then 'South'
+        when {{ state_column }} in ('BA','PE','CE','RN','PB','AL','SE','MA','PI') then 'Northeast'
+        when {{ state_column }} in ('AM','PA','RO','RR','AP','TO','AC') then 'North'
+        when {{ state_column }} in ('GO','MT','MS','DF') then 'Central-West'
+        else 'Unknown'
+    end
 
 {% endmacro %}
